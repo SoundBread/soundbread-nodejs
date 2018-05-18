4dc00603f2436e489a43d29bdbf213e900c89477
Chris van Marle <qistoph@gmail.com>
2018-05-18 09:37:47 +0200

    Add Brainstrom/Green Needle

--- core/sound/sounds.js
+++ core/sound/sounds.js
@@ -41,6 +41,7 @@ sounds = [
   {src: 'water.mp3', img: 'water.jpg', id: 'water', title: 'Water?', cost: 1},
   {src: 'buma.mp3', img: 'buma.jpg', id: 'buma', title: 'Dat is toch niet te geloven?', cost: 1},
   {src: 'goodmorning.mp3', img: 'goodmorning.jpg', id: 'goodmorning', title: 'Good Morning', cost: 1},
+  {src: 'greenbrain.mp3', img: 'greenbrain.jpg', id: 'greenbrain', title: 'Brainstorm', hidden: false},
   // Keep hidden: true at end to not show hints in shortcuts
   {src: 'maniacal.mp3', img: 'maniacal.jpg', id: 'maniacal', title: 'Maniacal', hidden:true},
   {src: 'gaaay.mp3', img: 'gaaay.jpg', id: 'gaaay', title: 'Gaaay', hidden: true},
