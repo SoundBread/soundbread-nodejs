400869942e9ee8c6f5509e36842325c59bc1209f
Chris van Marle <qistoph@gmail.com>
2019-07-19 15:14:48 +0200

    Check image sizes recursively

--- scripts/test-images.sh
+++ scripts/test-images.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 FAIL=0
-for FILE in webroot/img/*; do
-	identify -format '%i is %[fx:w]x%[fx:h] %C' "$FILE" | grep -v '190x190 JPEG' && FAIL=1
-done
+find webroot/img/ -type f -print0 |
+	while IFS= read -r -d '' FILE; do
+		identify -format '%i is %[fx:w]x%[fx:h] %C' "$FILE" | grep -v '190x190 JPEG' && FAIL=1
+	done
 exit $FAIL
